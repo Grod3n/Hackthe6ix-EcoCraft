aa5bd75bf2401d30ec1b79634977b09a0d6dab2a
LianAustin <austin.lian25@ycdsbk12.ca>
2024-08-03 11:23:50 -0400

    Attempted to make combat system
    Combat system kind of works, the player can definitely die and attack, but the attacks do no damage to the enemy.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,45 +2,56 @@ extends CharacterBody2D
 
 const speed = 100
 var current_dir = "none"
+var is_attacking = false
+var is_dead = false
+var health = 100
+var max_health = 100  
+
+@onready var attack_area = $AttackArea
+@onready var health_bar = $HealthBar  
 
 func _ready():
 	$AnimatedSprite2D.play("front_idle")
+	$AttackTimer.timeout.connect(Callable(self, "_on_AttackTimer_timeout"))
+	add_to_group("player") 
+	update_health_bar() # broken, dunno why
 
 func _physics_process(delta):
+	if is_dead:
+		return
 	player_movement(delta)
-#ssdf
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		attack()
+
 func player_movement(delta):
-	if Input.is_action_pressed("ui_right"):
-		current_dir = "right"
-		animate(1)
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		current_dir = "left"
-		animate(1)
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "down"
-		animate(1)
-		velocity.x = 0
-		velocity.y = speed
-	elif Input.is_action_pressed("ui_up"):
-		current_dir = "up"
-		animate(1)
-		velocity.x = 0
-		velocity.y = -speed
-	else:
-		animate(0)
-		velocity.x = 0
-		velocity.y = 0
-		
-	move_and_slide()
+	if not is_attacking:
+		velocity = Vector2.ZERO
+
+		if Input.is_action_pressed("ui_right"):
+			current_dir = "right"
+			animate(1)
+			velocity.x = speed
+		elif Input.is_action_pressed("ui_left"):
+			current_dir = "left"
+			animate(1)
+			velocity.x = -speed
+		elif Input.is_action_pressed("ui_down"):
+			current_dir = "down"
+			animate(1)
+			velocity.y = speed
+		elif Input.is_action_pressed("ui_up"):
+			current_dir = "up"
+			animate(1)
+			velocity.y = -speed
+		else:
+			animate(0)
+
+		move_and_slide()
 
 func animate(movement):
 	var dir = current_dir
 	var animation = $AnimatedSprite2D
-	
+
 	if dir == "right":
 		animation.flip_h = false
 		if movement == 1:
@@ -66,3 +77,52 @@ func animate(movement):
 			animation.play("front_walk")
 		else:
 			animation.play("front_idle")
+
+func attack():
+	is_attacking = true
+	var animation = $AnimatedSprite2D
+
+	if current_dir == "right":
+		animation.play("side_attack")
+	elif current_dir == "left":
+		animation.play("side_attack")
+	elif current_dir == "up":
+		animation.play("back_attack")
+	elif current_dir == "down":
+		animation.play("front_attack")
+
+	$AttackTimer.start(0.5)  
+
+	# Detect collision with enemy
+	for body in attack_area.get_overlapping_bodies():
+		if body.is_in_group("enemies"):
+			body.take_damage(10)  # broken for some reason
+			print("Dealing 10 damage to enemy: ", body)
+
+func _on_AttackTimer_timeout():
+	is_attacking = false
+
+func take_damage(amount):
+	health -= amount
+	update_health_bar()
+	print("Player health: ", health)
+	if health <= 0:
+		die()
+
+func update_health_bar():
+	if health_bar != null:
+		health_bar.value = health
+	else:
+		print("Health bar is null")
+
+func die():
+	is_dead = true
+	var animation = $AnimatedSprite2D
+	animation.play("death")
+	$AnimatedSprite2D.animation_finished.connect(Callable(self, "_on_death_animation_finished"))
+	print("Player has died")
+
+func _on_death_animation_finished():
+	if $AnimatedSprite2D.animation == "death":
+		queue_free()  # broken for reasons unknown to me
+		print("Player removed from scene")
