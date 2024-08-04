4fed7ffb40b1ad9a20feb47279e10109fda34842
terryxiao07 <131728510+terryxiao07@users.noreply.github.com>
2024-08-04 07:35:08 -0400

    added more enemy spawn areas

--- scripts/game.gd
+++ scripts/game.gd
@@ -11,9 +11,20 @@ func _ready():
 func _on_spawn_timer_timeout():
 	var Enemy = enemy.instantiate()
 	add_child(Enemy)
-	Enemy.position = $Spawn.position
 	
+	var location = RandomNumberGenerator.new().randi_range(1, 4)
+	
+	Enemy.position = $Spawn.position
 	var area = $SpawnArea
+	if (location == 1):
+		area = $SpawnArea
+	elif (location == 2):
+		area = $SpawnArea2
+	elif (location == 3):
+		area = $SpawnArea3
+	else:
+		area = $SpawnArea4
+
 	var position = area.position + Vector2(randf() * area.size.x, randf() * area.size.y)
 	$Spawn.position = position
 	
