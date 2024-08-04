71956a87ef62de47ca82802d16e2aff4de6fd1e7
terryxiao07 <131728510+terryxiao07@users.noreply.github.com>
2024-08-04 00:17:07 -0400

    made progressbar

---
+++ scripts/deteriorationBar.gd
@@ -0,0 +1,13 @@
+extends TextureProgressBar
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	update()
+
+func update():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
